8f5578d1901a1a0ea67919aee4f64ae1cb6dea39
Rayhan Muhammad Adha <67584512+Mr22XX@users.noreply.github.com>
2023-11-20 13:59:23 +0700

    Add files via upload

---
+++ ATM/src/src/atmBBC.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src;
+
+/**
+ *
+ * @author HP
+ */
+public class atmBBC {
+    
+   public static void main(String[] args){
+       home hm = new home();
+       hm.setVisible(true);
+       hm.pack();
+       hm.setLocationRelativeTo(null);
+       hm.setDefaultCloseOperation(home.EXIT_ON_CLOSE);
+       
+   }
+    
+}
